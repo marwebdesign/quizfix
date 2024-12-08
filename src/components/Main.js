82844ee6f8f263eea530ab04b015da103d3b4c2b
marwebdesign <maria_westling@hotmail.com>
2024-12-08 13:55:33 +0100

    setting up routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Quiz } from './Quiz';
+import { About } from './About';
+import { Form } from './Form';
+import { Home } from './Home';
 
 export const Main = () => {
   return (
-    <h1>❤️</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
